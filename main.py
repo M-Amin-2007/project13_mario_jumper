7d4a6e079a20ecdfd7a32fbac4045632c87c14da
M-Amin-2007 <mamp2007.mohammadi@gmail.com>
2023-04-24 23:16:01 +0430

    make obstacles and place them in screen.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """write by: Amin"""
 from ctypes import windll
+from random import choice
 from sys import modules
 import pgzrun
 import pygame
@@ -15,10 +16,16 @@ mod = modules["__main__"]
 # objects
 lands = [Actor("land_green", (land * 64 + 32, HEIGHT - 32)) for land in range(WIDTH // 64 + 1)]
 mario = Actor("p1", (50, HEIGHT - 91))
+obstacles = []
+for obs in range(4):
+    obstacle = Actor(choice(["obj1", "obj2", "obj3"]))
+    obstacle.midbottom = (obs * 400 + 500, HEIGHT - 64)
+    obstacles.append(obstacle)
 # main loop functions
 def draw():
     """draw everything in game here."""
     mod.screen.blit("sky2", (0, 0))
     for land_item in lands:land_item.draw()
     mario.draw()
+    for obstacle_item in obstacles:obstacle_item.draw()
 pgzrun.go()
