1a19d3d1867c0010daf8e1ff60db22109170941f
M-Amin-2007 <mamp2007.mohammadi@gmail.com>
2023-04-25 18:31:38 +0430

    make obstacles and lands movable.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ TITLE = "Mario Jumper 2023"
 hwnd = pygame.display.get_wm_info()["window"]
 windll.user32.MoveWindow(hwnd, 300, 50, WIDTH, HEIGHT, False)
 mod = modules["__main__"]
+speed = 5
 # objects
 lands = [Actor("land_green", (land * 64 + 32, HEIGHT - 32)) for land in range(WIDTH // 64 + 1)]
 mario = Actor("p1", (50, HEIGHT - 91))
@@ -25,7 +26,13 @@ for obs in range(4):
 def draw():
     """draw everything in game here."""
     mod.screen.blit("sky2", (0, 0))
-    for land_item in lands:land_item.draw()
+    for land_item in lands: land_item.draw()
     mario.draw()
-    for obstacle_item in obstacles:obstacle_item.draw()
+    for obstacle_item in obstacles: obstacle_item.draw()
+def update():
+    """update every thing in every frame."""
+    # move
+    for land_item in lands: land_item.x -= speed
+    for obstacle_item in obstacles: obstacle_item.x -= speed
+
 pgzrun.go()
