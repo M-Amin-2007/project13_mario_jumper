1504e98151bcf767a442767049da116afd05b04c
M-Amin-2007 <mamp2007.mohammadi@gmail.com>
2023-04-26 22:57:52 +0430

    mario simulation of running.

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ from sys import modules
 import pgzrun
 import pygame
 from pgzero.actor import Actor
-# functions
+# functions and classes
+class MyActor(Actor):
+    """make personalization the class."""
+    def __init__(self, image, pos=(-1000, -1000)):
+        super().__init__(image, pos)
+        self.images = [image]
+    def next_image(self):
+        """change to next image"""
+        idx = self.images.index(self.image)
+        self.image = self.images[(idx + 1) % len(self.images)]
 # variables and initialize
 WIDTH = 816
 HEIGHT = 600
@@ -13,13 +22,15 @@ TITLE = "Mario Jumper 2023"
 hwnd = pygame.display.get_wm_info()["window"]
 windll.user32.MoveWindow(hwnd, 300, 50, WIDTH, HEIGHT, False)
 mod = modules["__main__"]
-speed = 5
+speed = 10
+frame = 0
 # objects
-lands = [Actor("land_green", (land * 64 + 32, HEIGHT - 32)) for land in range(WIDTH // 64 + 2)]
-mario = Actor("p1", (50, HEIGHT - 91))
+lands = [MyActor("land_green", (land * 64 + 32, HEIGHT - 32)) for land in range(WIDTH // 64 + 2)]
+mario = MyActor("p1", (50, HEIGHT - 91))
+mario.images = ["p1", "p2", "p3"]
 obstacles = []
 for obs in range(4):
-    obstacle = Actor(choice(["obj1", "obj2", "obj3"]))
+    obstacle = MyActor(choice(["obj1", "obj2", "obj3"]))
     obstacle.midbottom = (obs * 400 + 500, HEIGHT - 64)
     obstacles.append(obstacle)
 # main loop functions
@@ -31,6 +42,8 @@ def draw():
     for obstacle_item in obstacles: obstacle_item.draw()
 def update():
     """update every thing in every frame."""
+    global frame
+    frame += 1
     # move
     for land_item in lands: land_item.x -= speed
     for obstacle_item in obstacles: obstacle_item.x -= speed
@@ -45,4 +58,7 @@ def update():
         if land_item.x < - land_item.width // 2:
             last_land = lands[lands.index(land_item) - 1]
             land_item.x = last_land.x + 64
+    # mario_part
+    if frame % (7 // speed ** 0.5) == 0:
+        mario.next_image()
 pgzrun.go()
